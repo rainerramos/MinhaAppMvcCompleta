c52105844f8602f4aae948865049e794660cd2af
Rainer <rainermr89@hotmail.com>
2022-07-26 14:47:31 -0300

    Scaffolding das Controllers e Views

---
+++ src/DevIO.App/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.App.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+
+    }
+}
--- src/DevIO.App/Data/ApplicationDbContext.cs
+++ src/DevIO.App/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DevIO.App.ViewModels;
 
 namespace DevIO.App.Data
 {
@@ -8,6 +9,7 @@ namespace DevIO.App.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
+        }        
+        public DbSet<DevIO.App.ViewModels.ProdutoViewModel>? ProdutoViewModel { get; set; }
     }
 }
